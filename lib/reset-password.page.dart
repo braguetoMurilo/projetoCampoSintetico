39e24c521592887398c606e55a246167ebc3e7e6
braguetoMurilo <murilobragueto@gmail.com>
2021-03-11 18:05:54 -0300

    Second commit!

--- lib/reset-password.page.dart
+++ lib/reset-password.page.dart
@@ -5,7 +5,7 @@ class ResetPasswordPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.white,
+        backgroundColor: Colors.white30,
         leading: IconButton(
           icon: Icon(Icons.arrow_back),
           color: Colors.black54,
@@ -13,12 +13,16 @@ class ResetPasswordPage extends StatelessWidget {
         ),
       ),
       body: Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+          image: AssetImage("images/futebool.jpg"),
+          fit: BoxFit.cover,
+        )),
         padding: EdgeInsets.only(
-            top: 60.0,
-            left: 40.0,
-            right:  40.0,
+          top: 25.0,
+          left: 40.0,
+          right: 40.0,
         ),
-        color: Colors.white,
         child: ListView(
           children: <Widget>[
             SizedBox(
@@ -34,14 +38,70 @@ class ResetPasswordPage extends StatelessWidget {
             Text(
               "Esqueceu sua senha ?",
               style: TextStyle(
-
+                fontSize: 32,
+                fontWeight: FontWeight.w800,
               ),
-
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Text(
+              "Por favor, informe o E-mail associado a sua conta que enviaremos um link para o mesmo com as instruções para restauração de sua senha.",
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w800,
+                color: Colors.black,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            TextFormField(
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                labelText: "E-mail",
+                labelStyle: TextStyle(
+                  color: Colors.black54,
+                  fontWeight: FontWeight.w700,
+                  fontSize: 25.0,
+                ),
+              ),
+              style: TextStyle(fontSize: 20),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Container(
+                height: 60.0,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color: Colors.red,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(5),
+                  ),
+                ),
+                child: SizedBox.expand(
+                  child: FlatButton(
+                    child: Text(
+                      "Enviar",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        fontSize: 25.0,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    onPressed: () {},
+                  ),
+                )),
+            SizedBox(
+              height: 20.0,
             ),
           ],
         ),
       ),
-
     );
   }
 }
