5d3b3cd5d79ae893a85ab90e13cb4d46961ba7f4
braguetoMurilo <murilobragueto@gmail.com>
2021-07-31 11:57:09 -0300

    Ajuste da tela de cadastro do jogador (Circulo de carregamento e bloqueio de campos)

--- lib/login.page.dart
+++ lib/login.page.dart
@@ -11,245 +11,242 @@ import 'package:projeto_campo/field.research.dart';
 import 'package:provider/provider.dart';
 
 class LoginPage extends StatelessWidget {
-
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passController = TextEditingController();
 
   final GlobalKey<FormState> formKey = GlobalKey<FormState>();
   final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
 
-
   @override
   Widget build(BuildContext context) {
-      return Provider(
-          create: (_) => UserManager(),
-        child: Scaffold(
-          key: scaffoldKey,
+    return Provider(
+      create: (_) => UserManager(),
+      child: Scaffold(
+        key: scaffoldKey,
         body: Center(
-        child: Container(
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage("images/futebool.jpg"),
-              fit: BoxFit.cover,
+          child: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("images/futebool.jpg"),
+                fit: BoxFit.cover,
+              ),
             ),
-          ),
-          child: Form(
-            key: formKey,
-            child: Consumer<UserManager>(
-              builder: (_, userManager,__){
-                return ListView(
-                  padding: EdgeInsets.only(
-                    top: 35.0,
-                    left: 40.0,
-                    right: 40.0,
-                  ),
-                  children: <Widget>[
-                    SizedBox(
-                      child: Image.asset(
-                        "images/logo.png",
-                        fit: BoxFit.none,
-                      ),
-                      width: 128.0,
-                      height: 128.0,
+            child: Form(
+              key: formKey,
+              child: Consumer<UserManager>(
+                builder: (_, userManager, __) {
+                  return ListView(
+                    padding: EdgeInsets.only(
+                      top: 35.0,
+                      left: 40.0,
+                      right: 40.0,
                     ),
-                    SizedBox(
-                      height: 40.0,
-                    ),
-                    TextFormField(
-                      controller: emailController,
-                      enabled: !userManager.loadind,
-                      keyboardType: TextInputType.emailAddress,
-                      decoration: InputDecoration(
-                        labelText: "E-mail ou Nome de usuário",
-                        labelStyle: TextStyle(
-                          color: Colors.black,
-                          fontWeight: FontWeight.w800,
-                          fontSize: 25.0,
+                    children: <Widget>[
+                      SizedBox(
+                        child: Image.asset(
+                          "images/logo.png",
+                          fit: BoxFit.none,
                         ),
+                        width: 128.0,
+                        height: 128.0,
                       ),
-                      style: TextStyle(
-                        fontSize: 20.0,
+                      SizedBox(
+                        height: 40.0,
                       ),
-                      validator: (email) {
-                        if (!emailValidad(email)) return 'E-mail inválido';
-                        return null;
-                      },
-                    ),
-                    SizedBox(
-                      height: 10.0,
-                    ),
-                    TextFormField(
-                      controller: passController,
-                      enabled: !userManager.loadind,
-                      keyboardType: TextInputType.text,
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        labelText: "Senha",
-                        labelStyle: TextStyle(
-                          color: Colors.black,
-                          fontWeight: FontWeight.w800,
-                          fontSize: 25.0,
-                        ),
-                      ),
-                      style: TextStyle(
-                        fontSize: 20.0,
-                      ),
-                      validator: (pass) {
-                        if (pass.isEmpty || pass.length < 6)
-                          return 'Senha Inválida';
-                        return null;
-                      },
-                    ),
-                    Container(
-                      height: 40.0,
-                      alignment: Alignment.center,
-                      child: FlatButton(
-                        child: Text(
-                          "Recuperar Senha",
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.w600,
+                      TextFormField(
+                        controller: emailController,
+                        enabled: !userManager.loadind,
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                          labelText: "E-mail ou Nome de usuário",
+                          labelStyle: TextStyle(
                             color: Colors.black,
+                            fontWeight: FontWeight.w800,
+                            fontSize: 25.0,
                           ),
                         ),
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => ResetPasswordPage(),
-                            ),
-                          );
+                        style: TextStyle(
+                          fontSize: 20.0,
+                        ),
+                        validator: (email) {
+                          if (!emailValidad(email)) return 'E-mail inválido';
+                          return null;
                         },
                       ),
-                    ),
-                    SizedBox(
-                      height: 40.0,
-                    ),
-                    Container(
-                      height: 60.0,
-                      alignment: Alignment.center,
-                      decoration: BoxDecoration(
-                        color: Colors.red,
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(5),
-                        ),
+                      SizedBox(
+                        height: 10.0,
                       ),
-                      child: SizedBox.expand(
+                      TextFormField(
+                        controller: passController,
+                        enabled: !userManager.loadind,
+                        keyboardType: TextInputType.text,
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          labelText: "Senha",
+                          labelStyle: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.w800,
+                            fontSize: 25.0,
+                          ),
+                        ),
+                        style: TextStyle(
+                          fontSize: 20.0,
+                        ),
+                        validator: (pass) {
+                          if (pass.isEmpty || pass.length < 6)
+                            return 'Senha Inválida';
+                          return null;
+                        },
+                      ),
+                      Container(
+                        height: 40.0,
+                        alignment: Alignment.center,
                         child: FlatButton(
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              Text(
-                                "Login",
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.white,
-                                  fontSize: 25.0,
-                                ),
-                              ),
-                            ],
+                          child: Text(
+                            "Recuperar Senha",
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w600,
+                              color: Colors.black,
+                            ),
                           ),
                           onPressed: () {
-                            if(formKey.currentState.validate()){
-                             userManager.signIn(
-                                user: User(
-                                  email: emailController.text,
-                                  password: passController.text,
-                                ),
-                                onFail: (e){
-                                  scaffoldKey.currentState.showSnackBar(
-                                    SnackBar(
-                                      content:  Text("Falha ao entrar: $e"),
-                                      backgroundColor: Colors.red,
-                                    ),
-                                  );
-                                },
-                                onSucces: (){
-                                  print('Sucesso');
-                                  Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                      builder: (context) => MultiFactor(),
-                                    ),
-                                  );
-                                },
-                              );
-                            };
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => ResetPasswordPage(),
+                              ),
+                            );
                           },
                         ),
                       ),
-                    ),
-                    SizedBox(
-                      height: 10.0,
-                    ),
-                    Container(
-                      height: 60.0,
-                      alignment: Alignment.centerRight,
-                      decoration: BoxDecoration(
-                        color: Colors.blueAccent,
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(5),
-                        ),
+                      SizedBox(
+                        height: 40.0,
                       ),
-                      child: SizedBox.expand(
+                      Container(
+                        height: 60.0,
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                          color: Colors.red,
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(5),
+                          ),
+                        ),
+                        child: SizedBox.expand(
                           child: FlatButton(
                             child: Row(
                               mainAxisAlignment: MainAxisAlignment.center,
                               children: <Widget>[
                                 Text(
-                                  "Facebook",
+                                  "Login",
                                   style: TextStyle(
                                     fontWeight: FontWeight.bold,
                                     color: Colors.white,
-                                    fontSize: 20,
+                                    fontSize: 25.0,
                                   ),
                                 ),
-                                /* Container(
+                              ],
+                            ),
+                            onPressed: () {
+                              if (formKey.currentState.validate()) {
+                                userManager.signIn(
+                                  user: User(
+                                    email: emailController.text,
+                                    password: passController.text,
+                                  ),
+                                  onFail: (e) {
+                                    scaffoldKey.currentState.showSnackBar(
+                                      SnackBar(
+                                        content: Text("Falha ao entrar: $e"),
+                                        backgroundColor: Colors.red,
+                                      ),
+                                    );
+                                  },
+                                  onSucces: () {
+                                    print('Sucesso');
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context) => MultiFactor(),
+                                      ),
+                                    );
+                                  },
+                                );
+                              }
+                              ;
+                            },
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10.0,
+                      ),
+                      Container(
+                        height: 60.0,
+                        alignment: Alignment.centerRight,
+                        decoration: BoxDecoration(
+                          color: Colors.blueAccent,
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(5),
+                          ),
+                        ),
+                        child: SizedBox.expand(
+                            child: FlatButton(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Text(
+                                "Facebook",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                  fontSize: 20,
+                                ),
+                              ),
+                              /* Container(
                       child: SizedBox(
                         child: Image.asset("images/icon.png"),
                         height: 28,
                         width: 28,
                       ),
                     ),*/
-                              ],
-                            ),
-                            onPressed: () {},
-                          )),
-                    ),
-                    SizedBox(
-                      height: 10.0,
-                    ),
-                    Container(
-                      height: 40.0,
-                      child: FlatButton(
-                        child: Text(
-                          "Cadastre-se",
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.w600,
-                            color: Colors.black,
+                            ],
                           ),
-                        ),
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: (context) => WhoAreYou()),
-                          );
-                        },
+                          onPressed: () {},
+                        )),
                       ),
-                    )
-                  ],
-                );
-              },
+                      SizedBox(
+                        height: 10.0,
+                      ),
+                      Container(
+                        height: 40.0,
+                        child: FlatButton(
+                          child: Text(
+                            "Cadastre-se",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w600,
+                              color: Colors.black,
+                            ),
+                          ),
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => WhoAreYou()),
+                            );
+                          },
+                        ),
+                      )
+                    ],
+                  );
+                },
+              ),
             ),
           ),
         ),
-        ),
-        ),
-      );
-
-
-
+      ),
+    );
   }
 }
--- lib/register.user.jogador.dart
+++ lib/register.user.jogador.dart
@@ -23,113 +23,131 @@ class FormPlayer extends StatelessWidget {
           margin: const EdgeInsets.symmetric(horizontal: 16),
           child: Form(
             key: formKey,
-            child: ListView(
-              padding: const EdgeInsets.all(16),
-              shrinkWrap: true,
-              children: <Widget>[
-                TextFormField(
-                  decoration: const InputDecoration(hintText: 'Nome Completo'),
-                  validator: (name) {
-                    if (name.isEmpty)
-                      return 'Campo Obrigatorio';
-                    else if (name.trim().split(' ').length <= 1)
-                      return 'Preencha seu nome completo';
-                    return null;
-                  },
-                  onSaved: (name) => user.name = name,
-                ),
-                const SizedBox(
-                  height: 16,
-                ),
-                TextFormField(
-                  decoration: const InputDecoration(hintText: 'E-mail'),
-                  keyboardType: TextInputType.emailAddress,
-                  validator: (email) {
-                    if (email.isEmpty)
-                      return 'Campo Obrigatorio';
-                    else if (!emailValidad(email)) return 'E-mail Inválido';
-                    return null;
-                  },
-                  onSaved: (email) => user.email = email,
-                ),
-                const SizedBox(
-                  height: 16,
-                ),
-                TextFormField(
-                  decoration: const InputDecoration(hintText: 'Senha'),
-                  obscureText: true,
-                  validator: (pass) {
-                    if (pass.isEmpty)
-                      return 'Campo obrigatorio';
-                    else if (pass.length < 6) return 'Senha muito curta';
-                    return null;
-                  },
-                  onSaved: (pass) => user.password = pass,
-                ),
-                const SizedBox(
-                  height: 16,
-                ),
-                TextFormField(
-                  decoration: const InputDecoration(hintText: 'Repita a Senha'),
-                  obscureText: true,
-                  validator: (pass) {
-                    if (pass.isEmpty)
-                      return 'Campo obrigatorio';
-                    else if (pass.length < 6) return 'Senha muito curta';
-                    return null;
-                  },
-                  onSaved: (pass) => user.confirmPassword = pass,
-                ),
-                const SizedBox(
-                  height: 16,
-                ),
-                SizedBox(
-                  height: 44,
-                  child: RaisedButton(
-                    color: Theme.of(context).primaryColor,
-                    disabledColor:
-                        Theme.of(context).primaryColor.withAlpha(100),
-                    textColor: Colors.white,
-                    onPressed: () {
-                      if (formKey.currentState.validate()) {
-                        formKey.currentState.save();
-
-                        if (user.password != user.confirmPassword) {
-                          scaffoldKey.currentState.showSnackBar(
-                            SnackBar(
-                              content: const Text("Senhas são diferentes"),
-                              backgroundColor: Colors.red,
-                            ),
-                          );
-                          return;
-                        }
-                        context.read<UserManager>().signUp(
-                          user: user,
-                          onSuccess: (){
-                            debugPrint("Sucesso");
-                            //TODO: POP
-
-                          },
-                          onFail: (e){
-                            scaffoldKey.currentState.showSnackBar(
-                              SnackBar(
-                                content:  Text("Falha ao cadastrar: $e"),
-                                backgroundColor: Colors.red,
-                              ),
-                            );
-                          }
-
-                        );
-
-                      }
-                    },
-                    child: const Text(
-                      'Criar Conta',
-                      style: TextStyle(fontSize: 18),
+            child: Consumer<UserManager>(
+              builder: (_,userManger, __){
+                return ListView(
+                  padding: const EdgeInsets.all(16),
+                  shrinkWrap: true,
+                  children: <Widget>[
+                    TextFormField(
+                      enabled: !userManger.loadind,
+                      decoration: const InputDecoration(hintText: 'Nome Completo'),
+                      validator: (name) {
+                        if (name.isEmpty)
+                          return 'Campo Obrigatorio';
+                        else if (name.trim().split(' ').length <= 1)
+                          return 'Preencha seu nome completo';
+                        return null;
+                      },
+                      onSaved: (name) => user.name = name,
                     ),
-                  ),
-                )
-              ],
+                    const SizedBox(
+                      height: 16,
+                    ),
+                    TextFormField(
+                      enabled: !userManger.loadind,
+                      decoration: const InputDecoration(hintText: 'E-mail'),
+                      keyboardType: TextInputType.emailAddress,
+                      validator: (email) {
+                        if (email.isEmpty)
+                          return 'Campo Obrigatorio';
+                        else if (!emailValidad(email)) return 'E-mail Inválido';
+                        return null;
+                      },
+                      onSaved: (email) => user.email = email,
+                    ),
+                    const SizedBox(
+                      height: 16,
+                    ),
+                    TextFormField(
+                      enabled: !userManger.loadind,
+                      decoration: const InputDecoration(hintText: 'Senha'),
+                      obscureText: true,
+                      validator: (pass) {
+                        if (pass.isEmpty)
+                          return 'Campo obrigatorio';
+                        else if (pass.length < 6) return 'Senha muito curta';
+                        return null;
+                      },
+                      onSaved: (pass) => user.password = pass,
+                    ),
+                    const SizedBox(
+                      height: 16,
+                    ),
+                    TextFormField(
+                      enabled: !userManger.loadind,
+                      decoration: const InputDecoration(hintText: 'Repita a Senha'),
+                      obscureText: true,
+                      validator: (pass) {
+                        if (pass.isEmpty)
+                          return 'Campo obrigatorio';
+                        else if (pass.length < 6) return 'Senha muito curta';
+                        return null;
+                      },
+                      onSaved: (pass) => user.confirmPassword = pass,
+                    ),
+                    const SizedBox(
+                      height: 16,
+                    ),
+                    SizedBox(
+                      height: 44,
+                      child: RaisedButton(
+                        color: Theme.of(context).primaryColor,
+                        disabledColor:
+                        Theme.of(context).primaryColor.withAlpha(100),
+                        textColor: Colors.white,
+                        onPressed: userManger.loadind ? null : () {
+                          if (formKey.currentState.validate()) {
+                            formKey.currentState.save();
+
+                            if (user.password != user.confirmPassword) {
+                              scaffoldKey.currentState.showSnackBar(
+                                SnackBar(
+                                  content: const Text("Senhas são diferentes"),
+                                  backgroundColor: Colors.red,
+                                ),
+                              );
+                              return;
+                            }
+                           userManger.signUp(
+                                user: user,
+                                onSuccess: (){
+                                  debugPrint("Sucesso");
+                                  scaffoldKey.currentState.showSnackBar(
+                                    SnackBar(
+                                      content: const Text("Cadastrado com sucesso !"),
+                                      backgroundColor: Colors.green,
+                                    ),
+                                  );
+
+
+                                },
+                                onFail: (e){
+                                  scaffoldKey.currentState.showSnackBar(
+                                    SnackBar(
+                                      content:  Text("Falha ao cadastrar: $e"),
+                                      backgroundColor: Colors.red,
+                                    ),
+                                  );
+                                }
+
+                            );
+
+                          }
+                        },
+                        child: userManger.loadind ?
+                        CircularProgressIndicator(
+                          valueColor: AlwaysStoppedAnimation(Colors.white),
+                        ) :
+                        const Text(
+                          'Criar Conta',
+                          style: TextStyle(fontSize: 18),
+                        ),
+                      ),
+                    )
+                  ],
+                );
+              },
             ),
           ),
         ),
